48bb506dc3b075c84c74d2140dfe2d0a577d04df
javaxd123 <marcinwarzybok@outlook.com>
2018-04-16 22:48:57 +0200

    before styled view

--- myblogkey/routes/middleware.js
+++ myblogkey/routes/middleware.js
@@ -21,7 +21,9 @@ exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
 		{ label: 'Blog', key: 'blog', href: '/blog' },
+		{ label: 'Store', key: 'store', href: '/products' },
 		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
+
 	];
 	res.locals.user = req.user;
 	next();
